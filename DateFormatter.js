f11581da793d3ff14d671c3bfab43217c34cd7f6
Christian Torres <chris_fito@hotmail.com>
2020-08-25 20:57:53 -0500

    var variables changed to let or const

--- DateFormatter.js
+++ DateFormatter.js
@@ -1,9 +1,9 @@
 function DateFormatter(date, format){
-    var months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio'
+    const months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio'
                  ,'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-    var shortMonths = ['Ene','Feb','Mar','Abr','May','Jun'
+    const shortMonths = ['Ene','Feb','Mar','Abr','May','Jun'
                       ,'Jul','Ago','Sep','Oct','Nov','Dic'];
-    var formatObj = {
+    const formatObj = {
         yyyy: () => {return date.getFullYear();},
         yy: () => {
             var strDate = date.getFullYear().toString(); 
@@ -27,11 +27,11 @@ function DateFormatter(date, format){
     const yearRegExp = /y{1,4}/;
     const dayRegExp = /d{1,2}/;
 
-    var yearFormat = format.match(yearRegExp);
-    var monthFormat = format.match(monthRegExp);
-    var dayFormat = format.match(dayRegExp);
+    const yearFormat = format.match(yearRegExp);
+    const monthFormat = format.match(monthRegExp);
+    const dayFormat = format.match(dayRegExp);
 
-    var result = format.replace(yearFormat,formatObj[yearFormat])
+    let result = format.replace(yearFormat,formatObj[yearFormat])
                     .replace(monthFormat,formatObj[monthFormat])
                     .replace(dayFormat,formatObj[dayFormat]);
     //console.log(`Format is ${format} This is yearFormat: ${yearFormat}, monthFormat: ${monthFormat} and dayFormat:${dayFormat}`);
